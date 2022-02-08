a05300339c1a9f62194d77986fe41f9e10d77351
Sean Hammell <sean.m.hammell@gmail.com>
2022-02-08 14:10:27 -0500

    Added buttons for selection and div tag for score to index.html. Refactored and added DOM manipulation to script.js.

--- script.js
+++ script.js
@@ -1,52 +1,57 @@
+let playerScore = 0;
+let computerScore = 0;
+
 function computerPlay () {
     let options = ["rock", "paper", "scissors"];
     return options[Math.floor(Math.random() * 3)];
 }
 
 function playRound (playerSelection, computerSelection) {
-    playerSelection = playerSelection.toLowerCase();
-    if (playerSelection === computerSelection) {
-        return 0;
-    }
-    else if (
-        (playerSelection === "rock" && computerSelection === "scissors")
-        || (playerSelection === "scissors" && computerSelection === "paper")
-        || (playerSelection === "paper" && computerSelection === "rock") 
+    if (
+        (playerSelection === "rock" && computerSelection === "scissors") ||
+        (playerSelection === "scissors" && computerSelection === "paper") ||
+        (playerSelection === "paper" && computerSelection === "rock") 
     ) {
-        return 1;
+        playerScore++;
     }
     else {
-        return -1;
+        computerScore++;
     }
+
+    if (playerScore === 5) {
+        alert("Player Wins!");
+        resetScore();
+    }
+    else if (computerScore === 5) {
+        alert("Computer Wins...");
+        resetScore();
+    }
+
+    const score = document.querySelector(".score");
+    score.textContent = `Player: ${playerScore} Computer: ${computerScore}`;
+
+}
+
+let resetScore = () => {
+    playerScore = 0;
+    computerScore = 0;
 }
 
 function game() {
-    gameWinner = 0;
-    for (let i = 0; i < 5; i++) {
-        console.log("Round " + (i + 1) + ":");
-        const playerSelection = prompt("Rock, Paper, or Scissors?");
-        const computerSelection = computerPlay();
-        roundWinner = playRound(playerSelection, computerSelection);
-        if (roundWinner === 1) {
-            console.log("You win! " + playerSelection.toLowerCase() + " beats " + computerSelection + ".");
-        }
-        else if (roundWinner === -1) {
-            console.log("You lose... " + computerSelection + " beats " + playerSelection.toLowerCase() + ".");
-        }
-        else {
-            console.log("Tie, you both chose " + computerSelection);
-        }
-        gameWinner += roundWinner;
-    }
-    if (gameWinner > 0) {
-        console.log("You win!");
-    }
-    else if (gameWinner < 0) {
-        console.log("You lose...");
-    }
-    else {
-        console.log("Tie");
-    }
+    const rock = document.querySelector("#rock");
+    rock.addEventListener('click', () => {
+        playRound("rock", computerPlay());
+    });
+
+    const paper = document.querySelector("#paper");
+    paper.addEventListener('click', () => {
+        playRound("paper", computerPlay());
+    });
+
+    const scissors = document.querySelector("#scissors");
+    scissors.addEventListener('click', () => {
+        playRound("scissors", computerPlay());
+    });
 }
 
 game();
